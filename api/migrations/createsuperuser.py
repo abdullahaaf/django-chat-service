f233bb6798f6bc931cb7c1980783e2f6c0d71766
abdullahaaf1 <abdullah.amin@majoo.id>
2023-02-11 11:21:50 +0700

    reconfigure migration for createsuperuser

--- api/migrations/createsuperuser.py
+++ api/migrations/createsuperuser.py
@@ -13,9 +13,16 @@ class Migration(migrations.Migration):
         DJANGO_SU_NAME = config('SUUSERNAME')
         DJANGO_SU_EMAIL = config('SUEMAIL')
         DJANGO_SU_PASSWORD = config('SUPASSWORD')
+        DJANGO_FIRST_NAME = config('FIRST_NAME'),
+        DJANGO_LAST_NAME = config('LAST_NAME'),
 
         superuser = User.objects.create_superuser(
             username=DJANGO_SU_NAME,
+            first_name=DJANGO_FIRST_NAME,
+            last_name=DJANGO_LAST_NAME,
+            is_superuser=True,
+            is_staff=True,
+            is_active=True,
             email=DJANGO_SU_EMAIL,
             password=DJANGO_SU_PASSWORD)
 
