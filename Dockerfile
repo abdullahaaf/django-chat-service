8e2a07d4614aec1dfe5dcb802eaa48c70aa80cd0
abdullahaaf1 <abdullah.amin@majoo.id>
2023-02-11 11:08:05 +0700

    fix wrong config in dockerfile and docker-compose

--- Dockerfile
+++ Dockerfile
@@ -23,8 +23,4 @@ COPY . $DockerHOME
 # run this command to install all dependencies  
 RUN pip install -r requirements.txt  
 # port where the Django app runs  
-EXPOSE 8000  
-# start server  
-CMD python manage.py makemigrations
-CMD python manage.py migrate
-CMD python manage.py runserver
+EXPOSE 8000 
--- api/migrations/createsuperuser.py
+++ api/migrations/createsuperuser.py
@@ -1,4 +1,4 @@
-import os
+from decouple import config
 from django.db import migrations
 
 class Migration(migrations.Migration):
@@ -10,9 +10,9 @@ class Migration(migrations.Migration):
     def generate_superuser(apps, schema_editor):
         from django.contrib.auth.models import User
 
-        DJANGO_SU_NAME = os.environ.get('SUUSERNAME')
-        DJANGO_SU_EMAIL = os.environ.get('SUEMAIL')
-        DJANGO_SU_PASSWORD = os.environ.get('SUPASSWORD')
+        DJANGO_SU_NAME = config('SUUSERNAME')
+        DJANGO_SU_EMAIL = config('SUEMAIL')
+        DJANGO_SU_PASSWORD = config('SUPASSWORD')
 
         superuser = User.objects.create_superuser(
             username=DJANGO_SU_NAME,
